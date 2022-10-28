5963791ab6c61e2ba98b0c735796bedd8a6c5929
itechdom <oalghnmi@gmail.com>
2022-10-28 10:44:12 +0300

    first commit

---
+++ src/test/kotlin/com/example/kotlineexpirments/KotlineExpirmentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlineexpirments
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlineExpirmentsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
