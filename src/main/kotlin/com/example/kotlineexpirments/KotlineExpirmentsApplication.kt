ce01cc003413f1499e4192c1bb6026de15b75a3a
itechdom <oalghnmi@gmail.com>
2022-11-05 12:09:43 +0300

    kotlin experiments

--- src/main/kotlin/com/example/kotlineexpirments/KotlineExpirmentsApplication.kt
+++ src/main/kotlin/com/example/kotlineexpirments/KotlineExpirmentsApplication.kt
@@ -2,9 +2,19 @@ package com.example.kotlineexpirments
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import reactor.core.publisher.Mono
 
 @SpringBootApplication
 class KotlineExpirmentsApplication
+
+ fun helloWorld(request: ServerRequest): Mono<ServerResponse> {
+		return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+			.body(BodyInserters.fromObject("Hello World!"));
+	}
 
 fun main(args: Array<String>) {
 	runApplication<KotlineExpirmentsApplication>(*args)
---
+++ src/main/kotlin/com/example/kotlineexpirments/config/HelloWorldRouter.kt
@@ -0,0 +1,19 @@
+package com.journaldev.spring.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class HelloWorldRouter {
+
+	@Bean
+	fun routeHelloWorld( helloWorldHandler: HelloWorldHandler):RouterFunction<ServerResponse> {
+		return RouterFunctions.route(RequestPredicates.GET("/helloWorld")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), helloWorldHandler.helloWorld());
+	}
+}
